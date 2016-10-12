656634b84179bd6f8e576ed1e6651f851c4cbf10
Mr. Mueller <bmueller@hstat.org>
2016-10-12 19:27:27 +0000

    change quiz to truthiness

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -4,69 +4,196 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
+        "name":    "Ruby Truthiness Quiz!!",
+        "main":    "<p>Think you understand Ruby Truthiness? Let's find out with this little quiz!</p>",
+        "results": "",
+        "level1":  "Truthiness Expert",
+        "level2":  "Truthiness Contender",
+        "level3":  "Truthiness Amateur",
+        "level4":  "Truthiness Newb",
         "level5":  "Stay in school, kid..." // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "false && true",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+        { // Question 2 - Multiple Choice, Single True Answer
+            "q": "true && true",
             "a": [
-                {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
-                {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
             ],
-            "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
         },
-        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+        { // Question 3 - Multiple Choice, Single True Answer
+            "q": "&quot;test&quot; == &quot;test&quot;",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
         },
-        { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+        { // Question 4 - Multiple Choice, Single True Answer
+            "q": "10 == 10 || 20 != 10",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
         },
-        { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+        { // Question 5 - Multiple Choice, Single True Answer
+            "q": "10 == 10 && 20 == 10",
             "a": [
-                {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
-        } // no comma here
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 6 - Multiple Choice, Single True Answer
+            "q": "true && 10 == 10",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 7 - Multiple Choice, Single True Answer
+            "q": "true || 10 == 10",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 8 - Multiple Choice, Single True Answer
+            "q": "false && 10 != 10",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 9 - Multiple Choice, Single True Answer
+            "q": "&quot;boolean&quot; == &quot;booleans&quot;",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 10 - Multiple Choice, Single True Answer
+            "q": "&quot;boolean&quot; != &quot;booleans&quot;",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 11 - Multiple Choice, Single True Answer
+            "q": "10 != 0 && 20 == 10",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 12 - Multiple Choice, Single True Answer
+            "q": "&quot;boolean&quot; == 1",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 13 - Multiple Choice, Single True Answer
+            "q": "!(true && false)",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 14 - Multiple Choice, Single True Answer
+            "q": "!(1000 == 1 || 10000 == 10000)",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 15 - Multiple Choice, Single True Answer
+            "q": "!(10 == 10 && 20 != 10)",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 16 - Multiple Choice, Single True Answer
+            "q": "!(1 != 100 || 30 == 40)",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 17 - Multiple Choice, Single True Answer
+            "q": "!(&quot;boolean&quot; == &quot;booleans&quot; && &quot;Code&quot; == &quot;Fun&quot;)",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 18 - Multiple Choice, Single True Answer
+            "q": "1 == 1 && (!(&quot;boolean&quot; == 1 || 1 == 0))",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        },
+        { // Question 19 - Multiple Choice, Single True Answer
+            "q": "&quot;strong&quot; == &quot;coffee&quot; && (!(30 == 40 || 30 == 30))",
+            "a": [
+                {"option": "true",      "correct": false},
+                {"option": "false",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> False.",
+            "incorrect": "<p><span>Umm no.</span></p> It's false." // no comma here
+        },
+        { // Question 20 - Multiple Choice, Single True Answer
+            "q": "30 == 30 && (!(&quot;boolean&quot; == &quot;booleans&quot; || &quot;Coding&quot; == &quot;Awesome&quot;))",
+            "a": [
+                {"option": "true",      "correct": true},
+                {"option": "false",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> True.",
+            "incorrect": "<p><span>Umm no.</span></p> It's true." // no comma here
+        }
+        
     ]
 };
